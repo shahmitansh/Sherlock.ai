c9843f920d7c33492e1613ee391375be83aceff4
Brian Shih <brianshihstart@gmail.com>
2017-01-21 16:05:08 -0800

    Initial Commit

---
+++ intervyou/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  intervyou
+//
+//  Created by Brian Shih on 1/21/17.
+//  Copyright © 2017 BrianShih. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
